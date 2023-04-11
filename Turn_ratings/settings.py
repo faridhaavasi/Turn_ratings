206dad4a8a99f40a2f4faaa861d1aaae6e523f6d
faridhaavasi <faridhavasi777@gmail.com>
2023-04-11 20:04:09 +0430

    dbuging

--- Turn_ratings/settings.py
+++ Turn_ratings/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my apps
     'web.apps.WebConfig',
-    'account.apps.AppConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
