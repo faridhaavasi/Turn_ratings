290c94ec34dae422c539f1b94b2dafd030bc8b82
faridhaavasi <faridhavasi777@gmail.com>
2023-04-14 18:42:42 +0430

    login view add method post

--- account/views.py
+++ account/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth import authenticate, login
 from .forms import Login_form
 
 class User_login(View):
     def get(self, request):
         form = Login_form()
+        return render(request, 'account/login.html', {'form': form})
+    def post(self, request):
+        form = Login_form(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['phone_number'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                form.add_error('phone_number', "اطلاعات صحیح نیست")
+        else:
+            form.add_error('phone_number', "طلاعات صحیح نیست")
         return render(request, 'account/login.html', {'form': form})
