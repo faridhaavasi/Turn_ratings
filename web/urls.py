d844d39744f74eb13a6a51c5a12108715a6a3f20
faridhaavasi <faridhavasi777@gmail.com>
2023-05-08 19:00:05 +0430

    used django generic view

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 app_name = 'web'
 urlpatterns = [
-    path('', views.home),
+    path('', views.Home.as_view(), name='home'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import View, edit
 # Create your views here.
 
-def home(request):
-
-    return render(request, 'web/index.html', {})
+class Home(View):
+    def get(self, request):
+        return render(request, 'web/index.html', {})
