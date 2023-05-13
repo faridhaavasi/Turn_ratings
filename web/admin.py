7aba739f49c2fc01c9534fd7e66752ac0ec2dbf9
faridhaavasi <faridhavasi777@gmail.com>
2023-05-13 17:59:10 +0430

    set model Turn

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Turn, Mont, Hour, Time
 # Register your models here.
+
+admin.site.register(Turn)
+admin.site.register(Time)
+admin.site.register(Mont)
+admin.site.register(Hour)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,26 @@
+import time
+
 from django.db import models
 
 # Create your models here.
+class Hour(models.Model):
+    hour = models.TimeField(time.thread_time_ns)
+    def __str__(self):
+        return f'{self.hour}'
+class Mont(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+class Time(models.Model):
+    yaer = models.IntegerField()
+    mont = models.ForeignKey(Mont, on_delete=models.CASCADE, related_name='time')
+    hour = models.ForeignKey(Hour, on_delete=models.CASCADE, related_name='time')
+    def __str__(self):
+        return f'{self.yaer}-{self.mont}={self.hour}'
+class Turn(models.Model):
+    number = models.IntegerField()
+    time = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='turn')
+
+    def __str__(self):
+        return f'{self.number}-{self.time}'
