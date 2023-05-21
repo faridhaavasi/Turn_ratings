1b6414e3d82066b9acb2372d79e8b9d83c6fff1a
faridhaavasi <faridhavasi777@gmail.com>
2023-05-21 20:49:49 +0330

    edit and senariu

--- web/models.py
+++ web/models.py
@@ -15,9 +15,11 @@ class Mont(models.Model):
 class Time(models.Model):
     yaer = models.IntegerField()
     mont = models.ForeignKey(Mont, on_delete=models.CASCADE, related_name='time')
+    day = models.IntegerField(max_length=31, null=True, blank=True)
+
     hour = models.ForeignKey(Hour, on_delete=models.CASCADE, related_name='time')
     def __str__(self):
-        return f'{self.yaer}-{self.mont}={self.hour}'
+        return f'{self.yaer}-{self.mont}-{self.day}={self.hour}'
 class Turn(models.Model):
     number = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='turn')
