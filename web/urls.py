9820f2949f632d170fe909064b0945f2bc4403ca
faridhaavasi <faridhavasi777@gmail.com>
2023-06-01 00:56:45 +0330

    debug

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,5 @@ app_name = 'web'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('Turnlist', views.TurnList.as_view(), name='TurnList'),
-    path('save_turn', views.Save_Turn.as_view(), name='save_turn'),
 
 ]
--- web/views.py
+++ web/views.py
@@ -2,25 +2,21 @@ from django.shortcuts import render, redirect
 from django.views.generic import View, ListView
 from .models import Turn
 
+
 # Create your views here.
 
 class Home(View):
     def get(self, request):
         return render(request, 'web/index.html', {})
 
+
 class TurnList(View):
+
     def get(self, request):
         turn = Turn.objects.all()
         return render(request, 'web/list_turn.html', context={'turn': turn})
 
-class Save_Turn(View):
-    def get(self, request):
-        turn = Turn.objects.all()
-        return render(request, 'web/list_turn.html', context={'turn': turn})
     def post(self, request):
         print(request.POST['mont'])
         if request.POST['mont']:
             return redirect('web:home')
-        return redirect('web:TurnList')
-
-
